474ff8f6769c62a9215e3203d669cf197a3d6eb8
luvwitch <itsalexkelly@gmail.com>
2021-01-15 13:13:51 -0800

    file updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
-import Saved from "./pages/Saved";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+// import Home from "./pages/Home";
+// import Saved from "./pages/Saved";
+// import NoMatch from "./pages/NoMatch";
+// import Nav from "./components/Nav";
 
 function App() {
   return (
     <Router>
       <div>
-        <Nav />
+        {/* <Nav />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/saved" component={Saved} />
           <Route component={NoMatch} />
-        </Switch>
+        </Switch> */}
+
+        :)
       </div>
     </Router>
   );
