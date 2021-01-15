90ca6b0e989bf6f3b5b3a9552038b9549a4b3963
luvwitch <itsalexkelly@gmail.com>
2021-01-15 14:18:25 -0800

    fleshing out components and pages, added routes to App.js

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import "./Nav.css";
+
+export default function Nav(){
+    return(
+        <div>
+            navbar?
+        </div>
+    )
+}
---
+++ client/src/pages/Login/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import "./Login.css";
+import {Col, Row, Container} from "../../components/Grid";
+
+export default function Login(){
+    return(
+        <div>
+            the login page
+        </div>
+    )
+}
