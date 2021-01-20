05aa224ef863a7b322baa95cfc594690ee0fc571
tylerdahl123 <dahlgren15@gmail.com>
2021-01-19 17:55:06 -0800

    I added some click functions and an intial event. I also added the ability to see the calendar in month,week,or day by installing the timeGrid and interactionplugin for fullcalendar

--- client/src/components/Calender/index.js
+++ client/src/components/Calender/index.js
@@ -1,16 +1,48 @@
-import React from "react";
+import React from 'react'
+import FullCalendar, { formatDate } from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+let eventid= 0
+let todayStr= new Date().toISOString().replace(/T.*S/,'')
 
+const INITIAL_EVENTS=[{
+  id: createEventId(),
+  title: "TEST",
+  start: todayStr
+}]
 
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from "@fullcalendar/daygrid"
+function createEventId(){
+  return String(eventid++)
+}
 
-export default class Calendar extends React.Component{
-  render(){
-    return(
-      <FullCalendar 
-      plugins={[dayGridPlugin]}
-      initialView="dayGridMonth"
-      />
+export default class Calendar extends React.Component {
+
+  state = {
+    currentEvents: []
+  }
+
+  render() {
+    return (
+    
+          <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            headerToolbar={{
+              left: 'prev,next today',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            }}
+            initialView='dayGridMonth'
+            editable={true}
+            selectable={true}
+            initialEvents={INITIAL_EVENTS} 
+          
+           
+          />
+       
     )
   }
+
+ 
+
 }
