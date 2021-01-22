e4297bde789de20950698c6a25422e0632c8a0ae
Kyle Gibson <kyle.gibson22@yahoo.com>
2021-01-22 11:20:04 -0800

    added color scheme btn

--- client/src/pages/Settings/index.js
+++ client/src/pages/Settings/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Card, Header, Title, Text, Footer, Body } from 'react-bootstrap';
+import { Button, Card, Header, Title, Text, Footer, Body, Dropdown, DropdownButton } from 'react-bootstrap';
 
 function Settings() {
     return (
@@ -7,11 +7,12 @@ function Settings() {
 <Card className="text-center">
   <Card.Header>Featured</Card.Header>
   <Card.Body>
-    <Card.Title>Special title treatment</Card.Title>
-    <Card.Text>
-      With supporting text below as a natural lead-in to additional content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+  <DropdownButton id="dropdown-basic-button" title="Color Scheme">
+  <Dropdown.Item href="#/action-1">Dark Mode</Dropdown.Item>
+  <Dropdown.Item href="#/action-2">Theme 1</Dropdown.Item>
+  <Dropdown.Item href="#/action-3">Theme 2</Dropdown.Item>
+  </DropdownButton>
+    
   </Card.Body>
   <Card.Footer className="text-muted">2 days ago</Card.Footer>
 </Card>
