23722afa654ae2d996ae4acf0c0c9b264df514cd
Kyle Gibson <kyle.gibson22@yahoo.com>
2021-01-22 15:41:01 -0800

    added css for settings dropdown button

--- client/src/pages/Settings/index.js
+++ client/src/pages/Settings/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Button, Card, Header, Title, Text, Footer, Body, Dropdown, DropdownButton } from 'react-bootstrap';
+import "./style.css"
 
 function Settings() {
     return (
         <div>
 <Card className="text-center">
-  <Card.Header>Featured</Card.Header>
+  <Card.Header>Customize</Card.Header>
   <Card.Body>
-  <DropdownButton id="dropdown-basic-button" title="Color Scheme">
+  <DropdownButton id="dropdown-basic-button" title="Color Scheme" style={{olor: "red"}}>
+    <br></br>
   <Dropdown.Item href="#/action-1">Dark Mode</Dropdown.Item>
   <Dropdown.Item href="#/action-2">Theme 1</Dropdown.Item>
   <Dropdown.Item href="#/action-3">Theme 2</Dropdown.Item>
