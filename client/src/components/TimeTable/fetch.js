ab44bc5da28d8ec1b9eb983dc3d565ff645c511b
tylerdahl123 <dahlgren15@gmail.com>
2021-01-25 14:38:14 -0800

    test

--- client/src/components/TimeTable/fetch.js
+++ client/src/components/TimeTable/fetch.js
@@ -2,13 +2,12 @@
 import request from "superagent";
 
 const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
-const CALENDAR_ID_UNIQUE = process.env.REACT_APP_CALENDAR_ID;
+// const CALENDAR_ID_UNIQUE = process.env.REACT_APP_CALENDAR_ID;
+const CALENDAR_ID = localStorage.getItem("email");
 
 
-console.log(GOOGLE_API_KEY)
-console.log(CALENDAR_ID_UNIQUE)
 
-let GOOGLE_CALENDAR_URL = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID_UNIQUE}/events?key=${GOOGLE_API_KEY}&singleEvents=true`;
+let GOOGLE_CALENDAR_URL = `https://www.googleapis.com/calendar/v3/calendars/dahlgren15@gmail.com/events?key=AIzaSyCL1pqTz4iiPIuP1_RB2NacTwUsuwc2LG8&singleEvents=true`;
 
     export function getEvents(callback) {
   request.get(GOOGLE_CALENDAR_URL).end((err, resp) => {
