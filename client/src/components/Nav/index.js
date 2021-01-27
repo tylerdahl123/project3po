4b7f4b5e0ee6052eee4ecdcf81977e5817e913de
Tito Rivera <titorivera0707@gmail.com>
2021-01-26 18:43:27 -0800

    Updating the styling on everything

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -8,23 +8,44 @@ function NavNew () {
   const[lightMode, setLightMode]= useState({
     lightOn:true
   });
+  const body = document.querySelector("body")
 
   const activateLight = e => {
-    e.preventDefault()
     setLightMode({...lightMode, 
       lightOn: lightMode.lightOn=true});
-      localStorage.setItem('lightOn', true);   
+      localStorage.setItem('lightOn', true);
   }
 
   const activateDark = e => {
-    e.preventDefault()
     setLightMode({...lightMode, 
       lightOn: lightMode.lightOn=false});
-      localStorage.setItem('lightOn', false);   
+      localStorage.setItem('lightOn', false); 
   }
 
+    const isLoggedIn = (localStorage.getItem('lightOn') === 'true');
 
-    // const isLoggedIn = (localStorage.getItem('login') === 'true');
+    useEffect(() => {
+    if(isLoggedIn) {
+      body.removeAttribute("style")
+      body.setAttribute("style", `background-color:rgb(243, 232, 232);
+      background-size:cover;
+      font-family:'Montserrat', sansSerif;
+      letter-spacing:.2rem;
+      background-image:url('https://coolbackgrounds.io/images/backgrounds/white/white-trianglify-b79c7e1f.jpg');
+      background-attachment:fixed;
+      background-position:center;`)
+    }else{
+      body.removeAttribute("style")
+      body.setAttribute("style", `background-color:black;
+      background-size:cover;
+      color:white;
+      font-family:'Montserrat', sansSerif;
+      letter-spacing:.2rem;
+      background-image:url('https://coolbackgrounds.io/images/backgrounds/black/black-radial-gradient-bb05ed79.jpg');
+      background-attachment:fixed;
+      background-position:center;`)
+    }
+  }, [isLoggedIn])
 
     // let showsettings;
 
@@ -48,7 +69,7 @@ function NavNew () {
 
     <div>
 
-      <Navbar bg="light" variant="light">
+      <Navbar bg={newStyle} variant={newStyle}>
         <Navbar.Brand href="/" >New App Name</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="/" >Home</Nav.Link>
